9a5861eb5c2b278441467adadad07b4b2e4ce1c4
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-07-13 07:35:33 +0530

    Add files via upload

---
+++ MEDIUM/sumwithout+-.py
@@ -0,0 +1,10 @@
+# platform - leetcode
+#link - https://leetcode.com/problems/sum-of-two-integers/
+# concept -  sum of two numbers without using  +,-  operators
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+       l=[a,b]
+       return sum(l)
+        
+
