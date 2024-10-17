060968a9290ed9f690522e142161fbad05440eb8
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-10-17 17:21:15 +0530

    Add files via upload

---
+++ MEDIUM/pow(x,n).py
@@ -0,0 +1,8 @@
+# platform - leetcode
+#link - https://leetcode.com/problems/powx-n/
+# concept - power of x,n
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
