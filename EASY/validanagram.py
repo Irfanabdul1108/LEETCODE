65cba492d849ebc9fae25599d3ac9e42c4c5c601
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-11-07 19:12:41 +0530

    Add files via upload

--- EASY/validanagram.py
+++ EASY/validanagram.py
@@ -1,14 +1,21 @@
 # platform - leetcode
-#link  - https://leetcode.com/problems/plus-one/
-# concept - check if both the strings are same or not (where one string is jumbeld and another string is not jumbeld)
+#link - https://leetcode.com/problems/valid-anagram/description/
+# concept - find the valid anagram
 
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        str1 = ''.join(map(str, digits))
-        m=int(str1)+1
-        return [
-        int(p) 
-        for p in str(m)
-        ]
-        
-        
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict1={}
+        dict2={}
+        for i in s:
+            if i in dict1:
+                dict1[i]+=1
+            else:
+                dict1[i]= dict1.get(i,0)+1
+        for i in t:
+                if i in dict2:
+                    dict2[i]+=1
+                else:
+                    dict2[i]= dict2.get(i,0)+1
+        if dict1==dict2:
+            return  True
+        return False
