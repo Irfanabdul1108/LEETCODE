e1cde50143b1bd924cc86f7b2a9950e82cb6782f
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-11-13 21:44:31 +0530

    Delete index-of-first-occ.py

--- index-of-first-occ.py
+++
@@ -1,8 +0,0 @@
-# platform - leetcode
-#link - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
-# concept -  Find the Index of the First Occurrence in a String
-
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
-        
