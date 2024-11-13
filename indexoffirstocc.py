c4bd4301cbdaf06113b837a155fe5c37b01ddeee
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-11-13 21:44:17 +0530

    Delete indexoffirstocc.py

--- indexoffirstocc.py
+++
@@ -1,8 +0,0 @@
-# platform - leetcode
-#link - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
-# concept - finding the index of the first occurrence of the string(heystack variable) in a string(needle variable)
-
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
-        
