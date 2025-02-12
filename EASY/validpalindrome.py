fc2f6f0db706849649c88802c7c7091912050a47
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2025-02-12 17:58:05 +0530

    Add files via upload

--- EASY/validpalindrome.py
+++ EASY/validpalindrome.py
@@ -1,18 +1,25 @@
 # platform - leetcode
 #link - https://leetcode.com/problems/valid-palindrome/
-# concept - somewhat diff from normal palindrome concept
+# concept - checking whether it is a valid palindrome or not
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        str1="abcdefghijklmnopqrstuvwxyz1234567890"
-        m=""
-        p=s.lower()
-        for i in p:
-            if i in str1:
-                m+=i
-        if m==m[::-1]:
-            return True
-        else:
-            return False
-
-        
+        i=0
+        n=len(s)
+        j=n-1
+        while(i<j):
+            if(s[i]==''):
+                i+=1
+            if(s[j]==''):
+                j-=1
+            if(s[i].isalnum()==False):
+                i+=1
+            if(s[j].isalnum()==False):
+                j-=1
+            if(s[i]!=''and s[j]!=''and s[i].isalnum()==True and s[j].isalnum()==True):
+                if (s[i].lower()==s[j].lower()):
+                    i+=1
+                    j-=1
+                else:
+                    return False
+        return True
